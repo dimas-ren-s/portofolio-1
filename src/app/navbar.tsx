007eac0220d88e2938519e6d0e5238a68283ae1b
Kelompok Gatau <dimassugarareal@gmail.com>
2024-03-04 13:05:41 +0700

    refactor(navbar): Create navigation hooks effect

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,29 +1,42 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 function makeNavbarListUI(list: any[]) {
-    return (<ul className="flex flex-row">
-        {
-            list.map(nav => (
-                <Link href={`/${nav.url}`} className="hover:text-blue-500"> {/* Kalau mau menambahkan / sebelum */}
-                    <li className="mx-2">
-                        {nav.nama}
-                    </li>
-                </Link>
-            ))
-        }
-    </ul >)
+    const pathname = usePathname();
+    const router = useRouter();
+    return (<div className="flex justify-between items-center">
+        <ul className="flex">
+            {
+                list.map(nav => (
+                    <Link href={nav.url} className={`${pathname === nav.url ? "text-blue-500" : ""} hover:text-blue-500`}> {/* Kalau mau menambahkan / sebelum */}
+                        <li className="mx-2">
+                            {nav.nama}
+                        </li>
+                    </Link>
+                ))
+            }
+        </ul >
+        <button onClick={() => router.push("https://github.com/dimas-ren-s/portofolio-1")} className="bg-[#309c4a] text-[#2b3137] px-2 h-8 rounded-lg shadow-md flex flex-row items-center">
+            <svg height="20" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true">
+                <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+            </svg>
+            <span>Github</span>
+        </button>
+    </div>)
 }
 
 export default function Navbar() {
     const listNavbar = [
         { nama: 'Home', url: '/' },
-        { nama: 'About', url: 'about' },
-        { nama: 'Project', url: 'project' },
-        { nama: 'Contact', url: 'contact' }
+        { nama: 'About', url: '/about' },
+        { nama: 'Projects', url: '/projects' },
+        { nama: 'Contact', url: '/contact' }
     ];
 
     return (
-        <nav className="bg-blue-200 p-4">
+        <nav className="bg-white p-4 fixed w-screen shadow-xl">
             {makeNavbarListUI(listNavbar)}
         </nav>
     );
